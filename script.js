e52f233284ff6396c6f54d18bbf02c21ce28a1bc
Prashant <Prashantsehgal03@gmail.com>
2024-03-20 08:53:51 +0530

    Page 1 done

--- script.js
+++ script.js
@@ -1,46 +1,74 @@
-const tl = gsap.timeline();
+function loadingScreen() {
+    const tl = gsap.timeline();
 
-tl.from(".line h1", {
-    y: 150,
-    stagger: 0.25,
-    duration: 0.6,
-    delay: 0.5,
-});
+    tl.from(".line h1", {
+        y: 150,
+        stagger: 0.25,
+        duration: 0.6,
+        delay: 0.5,
+    });
 
-tl.from("#line-part1", {
-    opacity: 0,
-    onStart: function () {
-        var h5 = document.querySelector("#line-part1 h5");
-        var grow = 0;
-        setInterval(() => {
-            if (grow < 100) {
-                h5.innerHTML = grow++;
-            } else {
-                h5.innerHTML = grow;
-            }
-        }, 34);
-    },
-});
+    tl.from("#line-part1", {
+        opacity: 0,
+        onStart: function () {
+            var h5 = document.querySelector("#line-part1 h5");
+            var grow = 0;
+            setInterval(() => {
+                if (grow < 100) {
+                    h5.innerHTML = grow++;
+                } else {
+                    h5.innerHTML = grow;
+                }
+            }, 34);
+        },
+    });
 
-tl.to(".line h2", {
-    animationName: 'anime',
-    opacity: 1,
-})
+    tl.to(".line h2", {
+        animationName: 'anime',
+        opacity: 1,
+    });
 
-tl.to("#loader", {
-    opacity: 0,
-    duration: 0.2,
-    delay: 4,
-});
+    tl.to("#loader", {
+        opacity: 0,
+        duration: 0.2,
+        delay: 0,
+    });
 
-tl.from("#page1", {
-    y: 1600,
-    opacity: 0,
-    delay: 0.2,
-    duration: 0.5,
-    ease: Power4
-})
+    tl.from("#page1", {
+        y: 1600,
+        opacity: 0,
+        delay: 0.2,
+        duration: 0.5,
+        ease: Power4
+    });
 
-tl.to("#loader", {
-    display: 'none',
-})
+    tl.to("#loader", {
+        display: 'none',
+    });
+
+    tl.from("#nav", {
+        opacity: 0,
+    });
+
+    tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1", {
+        y: 150,
+        stagger: 0.2
+    });
+}
+
+function cursorAnim() {
+    document.addEventListener("mousemove", function (dets) {
+        gsap.to('.crsr', {
+            left: dets.x,
+            top: dets.y,
+        });
+    });
+
+    Shery.makeMagnet("#nav-part2 h4", {
+        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+        duration: 0.3,
+    });
+}
+
+loadingScreen();
+cursorAnim();
